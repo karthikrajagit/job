39513220e20da9ea503c613c7dd985423c97ce2b
KarthikRaja0007 <karthik.raja.007c@gmail.com>
2025-01-08 11:10:58 +0530

    commit

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,24 +1,25 @@
-import React from 'react'
+import React from "react";
 
 export default function Intro() {
   return (
-    <div className='flex justify-between bg-blue-100'>
-       <img
-       src="https://nietm.in/wp-content/uploads/2022/11/CSE3.png"
-       alt="CSE3"
-       className='w-[600px] h-[400px]'
-       />
-       <div className='flex justify-center items-center flex-col mr-8'>
-       <h1 className='font-bold text-6xl text-blue-600'>
-            FIND YOUR DREAM 
-       </h1>
-       <h1 className='font-bold text-6xl'>
-            JOB
-       </h1>
-       <h1 className='font-bold text-6xl text-blue-600'>
-            EASILY AND QUICKLY 
-       </h1>
-       </div>
+    <div className="flex flex-col md:flex-row justify-between items-center bg-blue-100 ">
+      {/* Image Section */}
+      <img
+        src="https://nietm.in/wp-content/uploads/2022/11/CSE3.png"
+        alt="CSE3"
+        className="w-full h-[400px] md:w-[800px] md:h-full mb-6 md:mb-0 shadow-lg rounded-r-lg"
+      />
+
+      {/* Text Section */}
+      <div className="flex flex-col justify-center items-center text-center md:ml-8">
+        <h1 className="font-bold text-5xl md:text-5xl lg:text-6xl text-blue-600">
+          FIND YOUR DREAM
+        </h1>
+        <h1 className="font-bold text-5xl md:text-5xl lg:text-6xl">JOB</h1>
+        <h1 className="font-bold text-5xl md:text-5xl lg:text-6xl text-blue-600">
+          EASILY AND QUICKLY
+        </h1>
+      </div>
     </div>
-  )
+  );
 }
